1487e8be8b3868efe09f01e85f261c430cdf5c41
Philip Bergqvist <philip@pqvst.com>
2019-11-01 14:51:59 +0900

    Refactoring scripts and adding tests

---
+++ test/index.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const path = require('path');
+const Ajv = require('ajv');
+const ajv = new Ajv();
+
+const types = [
+  'Cafe',
+  'Bar',
+  'Restaurant',
+  'Lobby',
+  'Library',
+  'Coworking Space',
+  'Public Space',
+  'Event Space',
+  'Lounge',
+  'Booth'
+];
+
+const schema = {
+  type: 'array',
+  items: {
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+      name: { type: 'string' },
+      type: { type: 'string', enum: types },
+      area: { type: 'string' },
+      coordinates: { type: 'string' },
+      google_maps: { type: 'string' },
+      station: { type: 'string' },
+      wifi: { type: 'integer', minimum: 0, maximum: 5 },
+      speed: { type: 'integer' },
+      power: { type: 'number', minimum: 0, maximum: 5 },
+      seats: { type: 'number', minimum: 0, maximum: 5 },
+      comfort: { type: 'number', minimum: 1, maximum: 5 },
+      quiet: { type: 'number', minimum: 1, maximum: 5 },
+      drinks: { type: 'number', minimum: 0, maximum: 5 },
+      food: { type: 'number', minimum: 0, maximum: 5 },
+      price: { type: 'number', minimum: 1, maximum: 5 },
+      view: { type: 'number', minimum: 0, maximum: 5 },
+      toilets: { type: 'number', minimum: 0, maximum: 5 },
+      music: { type: 'boolean' },
+      smoking: { type: 'boolean' },
+      hours: { type: 'string' },
+      time_limit: { type: 'boolean' },
+      standing_tables: { type: 'boolean' },
+      outdoor_seating: { type: 'boolean' },
+      closed: { type: 'boolean' },
+      tips: { type: 'array', items: { type: 'string' } }
+    },
+    required: ['name', 'type', 'area', 'coordinates']
+  }
+};
+
+const cities = fs.readdirSync(path.join(__dirname, '../data/cities'));
+for (let city of cities) {
+  console.log('checking:', city);
+  const valid = ajv.validate(schema, require(path.join(__dirname, '../data/cities/', city)));
+  if (!valid) {
+    console.error(ajv.errors);
+    process.exit(-1);
+  } else {
+    console.log('ok!')
+  }
+}
