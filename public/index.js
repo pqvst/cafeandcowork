fe5df50c76de4e61427bc3e38dd3ca5dd9ed05c5
Philip Bergqvist <philip@pqvst.com>
2020-11-09 14:04:58 +0800

    Fixing maps and filtering
    Former-commit-id: ff7916dbd916010b8d1dc52fd9758e7686a1f5d5

--- public/index.js
+++ public/index.js
@@ -1,10 +1,5 @@
 (function () {
 
-  //----------------------------------------------------------------------
-  // Prepare mapbox
-  //----------------------------------------------------------------------
-  mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
-
   function colorRamp(value) {
     if (value >= 4) return '#418395';
     if (value >= 3) return '#2ecc71';
@@ -13,67 +8,96 @@
   }
 
   function makePopup(place) {
-    return `
-      <div>
-        <b><a target="_blank" href="${place.google_maps}">${place.name}</a></b>
-      </div>
-    `;
-    /*
-    <div>${this.formatScore(place._score)} (${place.type})</div>
-    ${place.tips ? place.tips.map(tip => `<div>${tip}</div>`).join('\n') : ''}
-    */
+    const html = `<div><b><a target="_blank" href="${place.url}">${place.name} (${place.score})</a></b></div>`;
+    return new mapboxgl.Popup({ offset: 25 }).setHTML(html);
   }
 
-  let map, markers = [];
+  mapboxgl.accessToken = 'pk.eyJ1IjoicHF2c3QiLCJhIjoiY2ptcTBnYnBjMTQ5bzNxbXB3YXk2NTdxMCJ9.siEO29S7-nsJbBiZ_jVhrg';
 
-  function createMap() {
+  var map;
+
+  window.CafeAndCowork = {};
+
+  window.CafeAndCowork.Place = function (place) {
+    var [lat, lng] = place.coordinates.split(',').map(Number);
     map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: COORDINATES ? COORDINATES.split(',').map(Number).reverse() : undefined,
+      center: [lng, lat],
+      zoom: 14
+    });
+    map.addControl(new mapboxgl.NavigationControl({ showCompass: false }));
+    map.on('load', function() {
+      new mapboxgl.Marker({ color: colorRamp(place.score), scale: 0.5 })
+        .setLngLat([lng, lat])
+        .setPopup(makePopup(place))
+        .addTo(map);
+    });
+  };
+
+  window.CafeAndCowork.City = function (coordinates, places) {
+    map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: coordinates ? coordinates.split(',').map(Number).reverse() : undefined,
       zoom: 10
     });
     map.addControl(new mapboxgl.NavigationControl({ showCompass: false }));
-    map.on('load', () => updateMap());
-  }
-
-  function getPlaces() {
-    const rows = Array.from(document.querySelectorAll('tbody tr'));
-    return rows.map(e => {
-      return {
-        score: e.getAttribute('data-score'),
-        coordinates: e.getAttribute('data-coordinates'),
-      };
-    });
-  }
-
-  function updateMap() {
-    if (!map || !map.loaded()) {
-      return createMap();
-    }
-    
-    markers.forEach(e => e.remove());
-    markers = [];
-
-    const bounds = new mapboxgl.LngLatBounds;
-    const places = getPlaces();
-    console.log(places);
-    for (const place of places) {
-      if (!place.coordinates) {
-        continue;
+    map.on('load', function() {
+      const bounds = new mapboxgl.LngLatBounds;
+      for (const place of places) {
+        if (!place.coordinates) {
+          continue;
+        }
+        const [lat, lng] = place.coordinates.split(',');
+        new mapboxgl.Marker({ color: colorRamp(place.score), scale: 0.5 })
+          .setLngLat([lng, lat])
+          .setPopup(makePopup(place))
+          .addTo(map);
+        bounds.extend([lng, lat]);
       }
-      const [lat, lng] = place.coordinates.split(',');
-      const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(makePopup(place));
-      const marker = new mapboxgl.Marker({ color: colorRamp(place.score), scale: 0.5 })
-        .setLngLat([lng, lat])
-        .setPopup(popup)
-        .addTo(map);
-      markers.push(marker);
-      bounds.extend([lng, lat]);
-    }
-    map.fitBounds(bounds, { padding: 50, linear: true, maxZoom: 17 });
-  }
+      map.fitBounds(bounds, { padding: 50, linear: true, maxZoom: 17 });
+    });
+  };
 
-  createMap();
+  window.CafeAndCowork.Table = function (table, input) {
+
+    input.addEventListener('input', function (evt) {
+      filter(evt.target.value.toLowerCase());
+    });
+    
+    table.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+      sort(th);
+    })));
+
+    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+    const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+        v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+        )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+    function sort(th) {
+      const table = th.closest('table');
+      const tbody = table.querySelector('tbody');
+      Array.from(tbody.querySelectorAll('tr'))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => tbody.appendChild(tr) );
+    }
+
+    function filterRow(tr, value) {
+      const include = !!Array.from(tr.children).find(e => e.innerText.toLowerCase().includes(value));
+      tr.style.display = include ? '' : 'none';
+    }
+
+    function filter(value) {
+      const tbody = table.querySelector('tbody');
+      Array.from(tbody.querySelectorAll('tr'))
+        .forEach(tr => filterRow(tr, value));
+    }
+
+    sort(document.querySelector('th'));
+    sort(document.querySelector('th'));
+
+  };
 
 })();
